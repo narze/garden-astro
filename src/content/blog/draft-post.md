542dba80086b3f4ebff2dd2b850629b00a61f524
Manassarn Manoonchai <manassarn@gmail.com>
2023-06-20 22:19:03 +0700

    Try draft post

---
+++ src/content/blog/draft-post.md
@@ -0,0 +1,9 @@
+---
+title: "This is draft post"
+description: "Lorem ipsum dolor sit amet"
+pubDate: "Jul 08 2022"
+heroImage: "/placeholder-hero.jpg"
+draft: true
+---
+
+You should not see this page on browser
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,22 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content"
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
-});
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+    draft: z.boolean().optional(),
+  }),
+})
 
-export const collections = { blog };
+export const collections = { blog }
