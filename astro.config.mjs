b3dc31f28e2466122edaaa4628bb557a8abd0afd
Manassarn Manoonchai <manassarn@gmail.com>
2023-06-27 23:28:12 +0700

    Add svelte with counter component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import githubFetchIntegration from "./src/lib/github-fetch-integration";
 
-import mdx from "@astrojs/mdx"
-
-import sitemap from "@astrojs/sitemap"
-import githubFetchIntegration from "./src/lib/github-fetch-integration"
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), githubFetchIntegration()],
-})
+  integrations: [mdx(), sitemap(), githubFetchIntegration(), svelte()]
+});
---
+++ src/components/Counter.svelte
@@ -0,0 +1,6 @@
+<!-- Svelte Counter -->
+<script>
+  export let count = 0
+</script>
+
+<button on:click={() => (count += 1)}>Svelte Counter: {count}</button>
