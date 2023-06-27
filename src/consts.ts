e3afe742b47b1b1037193a6746c021b8dfe89cf7
Manassarn Manoonchai <manassarn@gmail.com>
2023-06-27 22:51:08 +0700

    Cleanup menus

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "garden.narze.live"
+export const SITE_DESCRIPTION = "My notes & stuff"
+export const DEV = process.env.NODE_ENV === "development"
