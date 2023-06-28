563f2af379f00394b69c25fa225cabfd1c917569
Manassarn Manoonchai <manassarn@gmail.com>
2023-06-28 23:00:37 +0700

    Add remark plugin to re-link images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,30 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import githubFetchIntegration from "./src/lib/github-fetch-integration";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import githubFetchIntegration from "./src/lib/github-fetch-integration"
 
-import svelte from "@astrojs/svelte";
+import svelte from "@astrojs/svelte"
+
+function prependImageSrcPlugin() {
+  return (tree) => {
+    function visit(node) {
+      if (node.type === "image" && node.url) {
+        node.url = `/images/${node.url}`
+      }
+      if (node.children) {
+        node.children.forEach(visit)
+      }
+    }
+
+    visit(tree)
+  }
+}
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), githubFetchIntegration(), svelte()]
-});
+  integrations: [mdx(), sitemap(), githubFetchIntegration(), svelte()],
+  markdown: {
+    remarkPlugins: [prependImageSrcPlugin],
+  },
+})
