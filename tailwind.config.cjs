a8cccff4de67caa005f94a808c3f6dbbf94837ac
Manassarn Manoonchai <manassarn@gmail.com>
2023-06-30 23:48:04 +0700

    Add tailwind typography plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,9 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      padding: "1rem",
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 }
