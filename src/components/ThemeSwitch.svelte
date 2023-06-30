1932bec1765214b6d23c88468d2abca2ddfb122f
Manassarn Manoonchai <manassarn@gmail.com>
2023-06-30 23:34:06 +0700

    Add simple theme with dark mode switch

---
+++ src/components/ThemeSwitch.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  let theme = localStorage.getItem("theme") ?? "light"
+
+  $: {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+
+    localStorage.setItem("theme", theme)
+  }
+
+  function toggleTheme() {
+    theme = theme === "light" ? "dark" : "light"
+  }
+</script>
+
+<button on:click={toggleTheme}>{theme === "light" ? "🌞" : "🌙"}</button>
