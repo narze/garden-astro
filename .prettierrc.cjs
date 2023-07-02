b24181ce8d33752a268e096c9f3e7801320f61de
Manassarn Manoonchai <manassarn@gmail.com>
2023-07-02 19:17:11 +0700

    Configure Prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  // astroAllowShorthand: true,
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
