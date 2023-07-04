2843b4ef1b22976180dc751c27d9b8bca2d5c32c
Manassarn Manoonchai <manassarn@gmail.com>
2023-07-04 22:47:58 +0700

    Update rss feed

---
+++ src/pages/index.xml.ts
@@ -0,0 +1,2 @@
+// Re-export from index.xml.ts
+export { get } from "./rss.xml"
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,33 @@ import { getCollection } from "astro:content"
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts"
 
 export async function get(context) {
-  const posts = await getCollection("blog")
+  const entries = (await getCollection("second-brain")).sort(
+    (a, b) => b.data.date.valueOf() - a.data.date.valueOf()
+  )
+
+  const items = entries.map((post) => ({
+    title: post.data.title,
+    pubDate: post.data.date,
+    // description: post.data.description,
+    // customData: post.data.customData,
+
+    // Compute RSS link from post `slug`
+    // This example assumes all posts are rendered as `/blog/[slug]` routes
+    link: `/${post.slug}`,
+  }))
+
   return rss({
+    // `<title>` field in output xml
     title: SITE_TITLE,
+    // `<description>` field in output xml
     description: SITE_DESCRIPTION,
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#contextsite
     site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/blog/${post.slug}/`,
-    })),
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items,
+    // (optional) inject custom xml
+    customData: `<language>en-us</language>`,
   })
 }
