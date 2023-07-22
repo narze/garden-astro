f25ba0ca9a6f5b5dd901931b73ae6612164273b2
Manassarn Manoonchai <manassarn@gmail.com>
2023-07-22 17:47:09 +0700

    Exclude draft posts from rss feed

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -1,11 +1,15 @@
 import rss from "@astrojs/rss"
 import { getCollection } from "astro:content"
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts"
+import { DEV } from "../consts"
+import type { APIRoute } from "astro"
 
-export async function get(context) {
-  const entries = (await getCollection("second-brain")).sort(
-    (a, b) => b.data.date.valueOf() - a.data.date.valueOf()
-  )
+export const get: APIRoute = async (context) => {
+  const entries = (
+    await getCollection("second-brain", ({ data }) => {
+      return DEV ? true : data.draft !== true
+    })
+  ).sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
 
   const items = entries.map((post) => ({
     title: post.data.title,
@@ -25,7 +29,7 @@ export async function get(context) {
     description: SITE_DESCRIPTION,
     // Pull in your project "site" from the endpoint context
     // https://docs.astro.build/en/reference/api-reference/#contextsite
-    site: context.site,
+    site: String(context.site),
     // Array of `<item>`s in output xml
     // See "Generating items" section for examples using content collections and glob imports
     items,
